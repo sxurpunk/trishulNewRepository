cb7907b69dbcd1efdae1b15935d519c51a683631
surya <surya.iyer2004@gmail.com>
2025-01-29 14:45:12 -0500

    adding player dash

--- scripts/playerMovement.gd
+++ scripts/playerMovement.gd
@@ -62,6 +62,7 @@ func _physics_process(delta: float) -> void:
 	if Input.is_action_just_pressed("shoot"):
 		_shoot_projectile()
 	
+	
 		# Add the gravity.
 	if not is_on_floor():
 		velocity += get_gravity() * delta
@@ -88,6 +89,7 @@ func move(_delta):
 	else:
 		SPEED = walkSpeed
 		isRunning = false
+	
 	
 	if is_on_floor():
 		if direction:
