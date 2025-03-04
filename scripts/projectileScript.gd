60db34f80c49433b4b97f560469f69d0201da78b
surya <surya.iyer2004@gmail.com>
2025-03-04 18:27:21 -0500

    push

--- scripts/projectileScript.gd
+++ scripts/projectileScript.gd
@@ -3,7 +3,8 @@ extends Area3D
 @export var projectileSpeed = 10
 
 func _process(delta):
-	position+=transform.basis * Vector3(0, 0, -projectileSpeed) * delta
+	position += transform.basis * Vector3(0, 0, -projectileSpeed) * delta
+	
 #	var dir = get_tree().get_first_node_in_group("player").global_transform.basis.z.normalized()
 #	global_position += -dir * projectileSpeed * delta
 
